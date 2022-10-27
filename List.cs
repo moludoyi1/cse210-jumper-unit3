a345441c27527704797755984a765388460ca098
Michael <olu21003@byui.edu>
2022-10-27 14:45:04 -0600

    creating list

---
+++ List.cs
@@ -0,0 +1,10 @@
+// This program creates a list/dictionary for the jumper game
+
+class List{
+    public 
+    List<string> ActorList = new List<string> {""};
+    
+    List<string> Movies = new List<string> {""};
+
+    List<string> Hobbies = new List<string> {""};
+}
